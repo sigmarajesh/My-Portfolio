961db919cd02191e71e5493e25b5b3d2e2220b62
sigmarajesh <rajeshrangeela2005rr@gmail.com>
2024-12-04 12:29:53 +0530

    Init 🛩️

---
+++ components/About.jsx
@@ -0,0 +1,88 @@
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+import { socials } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import EmailIcon from "./../public/assets/icons/email.svg";
+
+function About() {
+  return (
+    <section
+      className="md:my-36 md:w-2/3 w-full h-full xl:ml-36 lg:ml-12 p-8 md:mt-[40svh] xl:mt-[150px]"
+      id="about"
+    >
+      <motion.div
+        variants={textVariant()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+      >
+        <p className={"sectionSubText text-gray-300"}>Introduction</p>
+        <h2 className={"sectionHeadText text-white"}>Overview.</h2>
+      </motion.div>
+      <motion.div
+        variants={fadeIn("", "", 0.1, 1)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className="mt-4 dark:text-ctnSecondaryDark text-gray-300 text-[17px] w-full leading-[30px] flex flex-col justify-between gap-6"
+      >
+        <div>
+        
+Hello, I'm Rajesh Kumar, a passionate Software Developer with a strong academic background in Software Engineering. I am currently pursuing my Bachelor's degree in Software Engineering from T.P. College, Madhepura. In my 2nd semester, I achieved an impressive score of 87.75%, with an overall CGPA of 9.2.
+
+As a Software Developer, I have hands-on experience with TypeScript, Tailwind CSS, and JavaScript. I specialize in frameworks such as React.js, Node.js, and Next.js, which enable me to create efficient and user-friendly web applications. My knowledge extends to fundamentals, data structures, and algorithms, making me proficient in both frontend and backend web development.
+
+I am also actively learning and expanding my expertise in Artificial Intelligence, a field that excites me greatly.
+          <br className="sm:block hidden" />
+          Let&apos;s collaborate to bring your ideas to life!
+        </div>
+        <div className="w-fit break-words">
+          <Link
+            href=""
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-primary w-full transition-all duration-100 ease-in flex md:items-center gap-2 md:flex-row flex-wrap word-break hover:-translate-y-2"
+          >
+            <EmailIcon className="w-[30px] h-[30px]" />
+            
+          </Link>
+        </div>
+        <div className="flex gap-5 items-center">
+          {socials.map((social) => (
+            <Link
+              href={social.link}
+              target="_blank"
+              key={social.id}
+              className="w-8 h-8 hover:-translate-y-2 ease-in transition-all duration-100 cursor-pointer"
+            >
+              {social.icon}
+            </Link>
+          ))}
+        </div>
+        <Link
+          href="document/Satyam-Kumar-Resume.pdf"
+          target="_blank"
+          rel="noreferrer"
+          className="w-fit"
+        >
+          <div className="btn w-fit bg-tertiary text-white px-7 py-2 rounded-md overflow-hidden relative cursor-pointer">
+            <div className="original bg-primary text-white px-7 py-2">
+              Resume
+            </div>
+            <div className="letters">
+              <span>R</span>
+              <span>e</span>
+              <span>s</span>
+              <span>u</span>
+              <span>m</span>
+              <span>e</span>
+            </div>
+          </div>
+        </Link>
+      </motion.div>
+    </section>
+  );
+}
+
+export default About;
